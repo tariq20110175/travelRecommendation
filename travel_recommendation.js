87f12478ec76a8a86adea581cd592f494a188e3e
tariq20110175 <tariq20110175@yahoo.com>
2024-12-03 12:57:02 -0500

    add enterkeypress to search input field

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -8,7 +8,15 @@ document.addEventListener('DOMContentLoaded', (event) => {
         searchInput.value = '';
         resultsDiv.innerHTML = '';
     });
-
+searchInput.addEventListener("keypress",function(event) {
+    // If the user presses the "Enter" key on the keyboard
+    if (event.key === "Enter") {
+      // Cancel the default action, if needed
+      //event.preventDefault();
+      // Trigger the button element with a click
+      searchButton.click();
+    }
+  });
     searchButton.addEventListener('click', () => {
         const query = searchInput.value.toLowerCase();
         fetch('travel_recommendation_api.json')
